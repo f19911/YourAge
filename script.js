1274c8c5f822c107db5725491411ccf3e75f3922
Dmitriy <67125062+f19911@users.noreply.github.com>
2020-06-25 17:03:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let yearOfBirth = prompt("Write down your year of birth");
+let year = 2020;
+let UserAge = year - yearOfBirth;
+
+if (yearOfBirth < 2020 && yearOfBirth > 1930) {
+
+    console.log('User age = ' + UserAge);
+
+} else {
+    console.log("Write down your year of birth");
+}
+}
+
+
+
+
+
+
+
+
